8352d3e0577a3e7228396512e13f199b80ec0a82
paulinho <orlandopaulo817@gmail.com>
2024-06-02 20:42:11 -0300

    router  with link

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function HomePage() {
   return (
     <div>
-        
+        <h1>hello</h1>
+        <Link to={"/login"}>produto</Link>
     </div>
   )
 }
